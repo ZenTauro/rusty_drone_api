8f6afc508091b4e0464bd5e18eb1bfcd579d02ad
zentauro <pedrogomezmartin1999@gmail.com>
2018-10-22 13:38:37 +0200

    Implementation of several endpoints

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,16 @@ pub struct Client {
 }
 
 impl Client {
+    /// Creates a new client for making requests
+    /// # Arguments
+    /// * `auth_token` - The authorization token provided in your account
+    /// * `base_url`   - The url with no path
+    /// 
+    /// # Examples
+    /// ```
+    /// let drone_client = Client::new("randomsupersecrettoken", dorne.example.org);
+    /// ```
+    /// note that there is no trasport in the url
     pub fn new(auth_token: String, base_url: String) -> Client {
         Client {
             auth_token: format!("Bearer {}",    auth_token),
@@ -23,11 +33,15 @@ impl Client {
         }
     }
 
+    fn get(&self, url: &String) -> String {
+        self.client.get(url)
+            .header("Authorization", self.auth_token.clone())
+            .send().unwrap().text().unwrap()
+    }
+
     pub fn get_current_user(&self) -> Result<UserInfo, serde_json::Error> {
         let url = format!("{}/user", self.base_url);
-        let serial = self.client.get(&url)
-            .header("Authorization", self.auth_token.clone())
-            .send().unwrap().text().unwrap();
+        let serial = self.get(&url);
         let user_info: UserInfo = serde_json::from_str(&serial)?;
 
         return Ok(user_info)
@@ -35,11 +49,34 @@ impl Client {
 
     pub fn get_current_user_repos(&self) -> Result<Vec<Repo>, serde_json::Error> {
         let url = format!("{}/user/repos", self.base_url);
-        let serial = self.client.get(&url)
-            .header("Authorization", self.auth_token.clone())
-            .send().unwrap().text().unwrap();
+        let serial = self.get(&url);
         let user_repos: Vec<Repo> = serde_json::from_str(&serial)?;
 
         return Ok(user_repos)
+    }
+
+    pub fn get_current_user_feed(&self) -> Result<Vec<FeedElement>, serde_json::Error> {
+        let url = format!("{}/user/feed", self.base_url);
+        let serial = self.get(&url);
+        let user_feed: Vec<FeedElement> = serde_json::from_str(&serial)?;
+
+        return Ok(user_feed)
+    }
+
+    pub fn get_users(&self) -> Result<Vec<UserInfo>, serde_json::Error> {
+        let url = format!("{}/users", self.base_url);
+        let serial = self.get(&url);
+        let users: Vec<UserInfo> = serde_json::from_str(&serial)?;
+
+        return Ok(users)
+    }
+
+    pub fn get_build_info(&self, owner: &String, repo: &String, build: &String) -> Result<Build, serde_json::Error> {
+        let url = format!("{}/repos/{owner}/{repo}/builds/{build}", self.base_url,
+            owner = owner, repo = repo, build = build);
+        let serial = self.get(&url);
+        let build: Build = serde_json::from_str(&serial)?;
+
+        return Ok(build)
     }
 }
--- src/structs.rs
+++ src/structs.rs
@@ -34,3 +34,115 @@ pub struct Repo {
     last_build: usize,
     config_file: String,
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct FeedElement {
+    #[serde(rename = "owner")]
+    owner: String,
+    #[serde(rename = "name")]
+    name: String,
+    #[serde(rename = "full_name")]
+    full_name: String,
+    #[serde(rename = "number")]
+    number: i64,
+    #[serde(rename = "event")]
+    event: Event,
+    #[serde(rename = "status")]
+    status: Status,
+    #[serde(rename = "created_at")]
+    created_at: i64,
+    #[serde(rename = "started_at")]
+    started_at: i64,
+    #[serde(rename = "finished_at")]
+    finished_at: i64,
+    #[serde(rename = "commit")]
+    commit: String,
+    #[serde(rename = "branch")]
+    branch: String,
+    #[serde(rename = "ref")]
+    test_ref: String,
+    #[serde(rename = "message")]
+    message: String,
+    #[serde(rename = "author")]
+    author: String,
+    #[serde(rename = "author_avatar")]
+    author_avatar: String,
+    #[serde(rename = "author_email")]
+    author_email: String,
+    #[serde(rename = "refspec")]
+    refspec: Option<String>,
+    #[serde(rename = "title")]
+    title: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Event {
+    #[serde(rename = "pull_request")]
+    PullRequest,
+    #[serde(rename = "push")]
+    Push,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Status {
+    #[serde(rename = "failure")]
+    Failure,
+    #[serde(rename = "success")]
+    Success,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Build {
+    #[serde(rename = "id")]
+    id: i64,
+    #[serde(rename = "number")]
+    number: i64,
+    #[serde(rename = "parent")]
+    parent: i64,
+    #[serde(rename = "event")]
+    event: String,
+    #[serde(rename = "status")]
+    status: Status,
+    #[serde(rename = "created_at")]
+    created_at: i64,
+    #[serde(rename = "enqueued_at")]
+    enqueued_at: i64,
+    #[serde(rename = "started_at")]
+    started_at: i64,
+    #[serde(rename = "finished_at")]
+    finished_at: i64,
+    #[serde(rename = "deploy_to")]
+    deploy_to: String,
+    #[serde(rename = "commit")]
+    commit: String,
+    #[serde(rename = "branch")]
+    branch: String,
+    #[serde(rename = "ref")]
+    build_ref: String,
+    #[serde(rename = "remote")]
+    remote: String,
+    #[serde(rename = "title")]
+    title: String,
+    #[serde(rename = "message")]
+    message: String,
+    #[serde(rename = "timestamp")]
+    timestamp: i64,
+    #[serde(rename = "sender")]
+    sender: String,
+    #[serde(rename = "author")]
+    author: String,
+    #[serde(rename = "author_avatar")]
+    author_avatar: String,
+    #[serde(rename = "author_email")]
+    author_email: String,
+    #[serde(rename = "link_url")]
+    link_url: String,
+    #[serde(rename = "signed")]
+    signed: bool,
+    #[serde(rename = "verified")]
+    verified: bool,
+    #[serde(rename = "reviewed_by")]
+    reviewed_by: String,
+    #[serde(rename = "reviewed_at")]
+    reviewed_at: i64,
+}
